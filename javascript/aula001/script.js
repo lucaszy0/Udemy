0742acf84c24573cedfd6a6a8497e419b41a99fb
lucas gabriel <gabriel.cardosso13@gmail.com>
2025-05-18 21:06:13 -0300

    2.0

--- javascript/aula001/script.js
+++ javascript/aula001/script.js
@@ -3,6 +3,6 @@ selecionar um elemento no dom
 atualizar o valor desse elemento com uma string*/
 document.getElementById("nome").value = "oi"
 //pode ser utilizada no case sensitive
-var nome , var AulaVariaveis , var _idade
-/* nao pode ser utilizada no case sensitive
+/* var nome , var AulaVariaveis , var _idade
+nao pode ser utilizada no case sensitive
 var 123nome , var numero */
